c63b29483ba585ffa69fa6063d6b018d84a1ad48
Jeong Seon U <sw.jeong97@gmail.com>
2024-10-14 12:11:37 +0900

    feat : 양의 정수(0포함)를 입력받기

---
+++ src/main/java/com/example/calculator/App.java
@@ -0,0 +1,15 @@
+package com.example.calculator;
+
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("첫 번째 숫자를 입력하세요: ");
+        int a = sc.nextInt();
+
+        System.out.println("두 번째 숫자를 입력하세요: ");
+        int b = sc.nextInt();
+    }
+}
