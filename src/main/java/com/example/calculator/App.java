1f6f2c0c2d74969ce6a59f21cae82b76752fc01d
Jeong Seon U <sw.jeong97@gmail.com>
2024-10-14 12:14:14 +0900

    feat : 사칙연산 기호를 입력받기

--- src/main/java/com/example/calculator/App.java
+++ src/main/java/com/example/calculator/App.java
@@ -11,5 +11,10 @@ public class App {
 
         System.out.println("두 번째 숫자를 입력하세요: ");
         int b = sc.nextInt();
+
+        System.out.println("사칙연산 기호를 입력하세요:");
+        String op = sc.next();
+
+
     }
 }
